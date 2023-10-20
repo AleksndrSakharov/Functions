d863a5418960695fe289059e670c911338b71c0d
Aleksandr_Sakharov <mr.11aleksandrsasharov11@mail.ru>
2023-10-20 13:59:06 +0300

    2

--- main.c
+++ main.c
@@ -17,10 +17,15 @@ int multy(int a, int b);
 
 
 int main(){
+    // double third = 1.0;
+    // {
+        // int first = 10, second = 11;
+    // }
     double arr[] = {1, 2, 3, 4, 5, 6, 7};
     print(arr, sizeof(arr)/sizeof(double));
     int result123 = multy(50, 100);
     printf("%d\n",result123);
+    // printf("%d %d %lf"), first, second, third;
     return 0;
 }
 
